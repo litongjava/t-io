41caa75b31e91d74bd22bb045f218aed5f297608
litongjava <litongjava@qq.com>
2023-12-10 09:45:59 -1000

    add tio-http-common

---
+++ tio-http-common/src/main/java/com/litongjava/tio/http/common/utils/HttpDateTimer.java
@@ -0,0 +1,37 @@
+package com.litongjava.tio.http.common.utils;
+
+import org.tio.utils.SystemTimer;
+import org.tio.utils.SystemTimer.TimerListener;
+import org.tio.utils.hutool.DateUtil;
+
+import com.litongjava.tio.http.common.HeaderValue;
+
+/**
+ * 
+ * @author tanyaowu 
+ * 2018年6月17日 下午10:37:16
+ */
+public class HttpDateTimer {
+
+  static {
+    SystemTimer.addTimerListener(new TimerListener() {
+      @Override
+      public void onChange(long currTime) {
+        httpDateString = DateUtil.httpDate(currTime);
+        httpDateValue = HeaderValue.from(httpDateString);
+      }
+    });
+  }
+
+  private static volatile String httpDateString = DateUtil.httpDate();
+
+  public static volatile HeaderValue httpDateValue = HeaderValue.from(httpDateString);
+
+  public static String currDateString() {
+    return httpDateString;
+  }
+
+  public static HeaderValue httpDateValue() {
+    return httpDateValue;
+  }
+}
