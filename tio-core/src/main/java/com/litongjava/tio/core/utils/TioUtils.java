7aae967540041860cd0d4dff12025fd34f6a020b
litongjava <litongjava@qq.com>
2024-09-20 13:30:23 -1000

    release to 3.7.3.v20240917-RELEASE

--- tio-core/src/main/java/com/litongjava/tio/core/utils/TioUtils.java
+++ tio-core/src/main/java/com/litongjava/tio/core/utils/TioUtils.java
@@ -33,17 +33,17 @@ public class TioUtils {
             log.error(e.toString(), e);
           }
         }
-        log.info("{}, isopen:{}, isClosed:{}, isRemoved:{}", channelContext, isopen, channelContext.isClosed, channelContext.isRemoved);
+        //log.info("{}, isopen:{}, isClosed:{}, isRemoved:{}", channelContext, isopen, channelContext.isClosed, channelContext.isRemoved);
         return false;
       }
     } else {
-      log.error("{}, 请检查此异常, asynchronousSocketChannel is null, isClosed:{}, isRemoved:{}, {} ", channelContext, channelContext.isClosed, channelContext.isRemoved, TioThreadUtils.stackTrace());
+      log.error("{}, Plese check, asynchronousSocketChannel is null, isClosed:{}, isRemoved:{}, {} ", channelContext, channelContext.isClosed, channelContext.isRemoved, TioThreadUtils.stackTrace());
       return false;
     }
 
     if (!isopen) {
-      log.info("{}, 可能对方关闭了连接, isopen:{}, isClosed:{}, isRemoved:{}", channelContext, isopen, channelContext.isClosed, channelContext.isRemoved);
-      Tio.close(channelContext, "asynchronousSocketChannel is not open, 可能对方关闭了连接", CloseCode.CHANNEL_NOT_OPEN);
+      log.info("connection might close by peer,{}, isOpen:{}, isClosed:{}, isRemoved:{}", channelContext, isopen, channelContext.isClosed, channelContext.isRemoved);
+      Tio.close(channelContext, "asynchronousSocketChannel is not open, connection might close by peer", CloseCode.CHANNEL_NOT_OPEN);
       return false;
     }
     return true;
